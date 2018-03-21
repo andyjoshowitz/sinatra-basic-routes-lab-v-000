d07918163155051a431fd8cc7fe9f9fe583c8dec
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-21 17:05:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,15 @@ class App < Sinatra::Base
   
   get '/name' do
     "My name is __"
-    erb :'medicines/index.html.erb'
+    erb :'name/index.html.erb'
   end
   
+  get '/hometown' do
+    "My hometown is __"
+    erb :'hometown/index.html.erb'
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
